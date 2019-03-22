86096a32a5757feb05c2174b7f657f43fbaf17e2
Lilu <KHussell@gmail.com>
2019-03-22 18:30:37 -0400

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,45 @@
-var wins=0,
-    losses= 0,
-    guessesLeft=9,
-    guessesSoFar,
-    userGuess,
-    letterChoices= ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-    computerLetter
+var wins=0
+var losses= 0
+var guessesLeft=9
+var guessesSoFar =""
+var userGuess
+var letterChoices= ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var computerLetter
     
 
-var winsText=document.getElementById('wins'),
-    lossesText=document.getElementById('losses'),
-    guessesLeftText=document.getElementById('guessesLeft'),
-    guessesSoFarText=document.getElementById('guessesSoFar')
+var winsText = document.getElementById('wins')
+var lossesText = document.getElementById('losses')
+var guessesLeftText = document.getElementById('guessesLeft')
+var guessesSoFarText = document.getElementById('guessesSoFar')
 
-winsText.innerText="Wins: 0"
-lossesText.textContent="Losses: 0"
-guessesLeftText.textContent="Guesses Left: 9"
-guessesSoFarText.textContent= "Guessed Already: "
+winsText.textContent="Wins: " +0
+lossesText.textContent="Losses: " +0
+guessesLeftText.textContent="Guesses Left: " +9 
+guessesSoFarText.textContent= "Guessed Already: none "
 
 
 document.onkeyup= function(event){
    userGuess=event.key
-   computerLetter= letterChoices[Math.floor(Math.random()*letterChoices.length)]
-   if(userGuess === computerLetter){
-       guessesLeft=9
-       wins += 1
-       winsText.textContent="Wins: " + wins
-       guessesLeft.textContent= "Guesses Left: "+ guessesLeft
-   } else if (userGuess !== computerLetter && guessesLeft === 0){  
-           losses+= 1
-           lossesText.textContent= "Losses: " + losses
-           guessesLeftText.textContent="Guesses Left: "+ guessesLeft
-       
-       }else{
+
+   if(guessesLeft === 9){
+     computerLetter= letterChoices[Math.floor(Math.random()*letterChoices.length)]
+    }
+   if (userGuess !== computerLetter && guessesLeft === 1){  
+    losses+= 1
+    lossesText.textContent= "Losses: " + losses
+    guessesLeft= 9
+    guessesLeftText.textContent="Guesses Left: " + guessesLeft
+    guessesSoFarText.textContent="Guessed Already: none"   
+   } else if(userGuess === computerLetter){
+    wins += 1
+    winsText.textContent="Wins: " + wins
+    guessesLeft=9
+    guessesLeftText.textContent= "Guesses Left: "+ guessesLeft
+    }else{
         guessesLeft-= 1
         guessesLeftText.textContent="Guesses Left: "+ guessesLeft
         guessesSoFar+= userGuess
-        guessesSoFarText.textConent= "Guessed Already: " + guessesSoFar
+        guessesSoFarText.textContent= "Guessed Already: " + guessesSoFar
        }
     
    
