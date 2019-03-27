e55a8cf41b5c0925fc1ac1318011a01e67d9637b
Lilu <KHussell@gmail.com>
2019-03-27 13:59:28 -0400

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,84 @@
-var wins=0
-var losses= 0
-var guessesLeft=9
-var guessesSoFar =""
-var userGuess
-var letterChoices= ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var computerLetter
+//initializing variables
+var wins=0,
+  losses= 0,
+  guessesLeft=9,
+  guessesSoFar ="",
+  userGuess,
+  letterChoices= ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  computerLetter
     
-
+//connecting to html
 var winsText = document.getElementById('wins')
 var lossesText = document.getElementById('losses')
 var guessesLeftText = document.getElementById('guessesLeft')
 var guessesSoFarText = document.getElementById('guessesSoFar')
 
+
+//initializing displayed text content
 winsText.textContent="Wins: " +0
 lossesText.textContent="Losses: " +0
 guessesLeftText.textContent="Guesses Left: " +9 
 guessesSoFarText.textContent= "Guessed Already: none "
 
 
-document.onkeyup= function(event){
-   userGuess=event.key
+//object with game functions
+var game={
 
-   if(guessesLeft === 9){
-     computerLetter= letterChoices[Math.floor(Math.random()*letterChoices.length)]
-    }
-   if (userGuess !== computerLetter && guessesLeft === 1){  
+  computerPlayer: function(){
+    //when key is pressed and all guesses remain a random computer choice will be established
+    computerLetter= letterChoices[Math.floor(Math.random()*letterChoices.length)]
+  },
+
+  lost: function(){
+    //if 1 last guesse and userGuess does not equal computer choice:
+    //losses increased by one
+    //guessesLeft, guessesSoFar, and text contents all re-initialized
     losses+= 1
     lossesText.textContent= "Losses: " + losses
     guessesLeft= 9
     guessesLeftText.textContent="Guesses Left: " + guessesLeft
     guessesSoFar=""
-    guessesSoFarText.textContent="Guessed Already: none"   
-   } else if(userGuess === computerLetter){
+    guessesSoFarText.textContent="Guessed Already: none"
+  },
+
+  won: function(){
+    //if userGuess equals computer choice:
+    //wins increased by one
+    //guessesLeft, guessesSoFar, and text contents all re-initialized
     wins += 1
     winsText.textContent="Wins: " + wins
     guessesLeft=9
     guessesLeftText.textContent= "Guesses Left: "+ guessesLeft
     guessesSoFar= ""
     guessesSoFarText.textContent= "Guessed Already: none"
-    }else{
-        guessesLeft-= 1
-        guessesLeftText.textContent="Guesses Left: "+ guessesLeft
-        guessesSoFar+= userGuess
-        guessesSoFarText.textContent= "Guessed Already: " + guessesSoFar
-       }
-    
-   
+  },
+
+  wrongGuess: function(){
+    //if userGuess does not equal computer choice and guesses are still left:
+    //guesses go down by 1
+    //guessesAlready adds the userGuess to it's string
+    //text contents updated with results
+    guessesLeft-= 1
+    guessesLeftText.textContent="Guesses Left: "+ guessesLeft
+    guessesSoFar+= userGuess
+    guessesSoFarText.textContent= "Guessed Already: " + guessesSoFar
+  }
+}
 
 
+//start of game
+document.onkeyup= function(event){
+   userGuess=event.key
 
+   if(guessesLeft === 9){
+     game.computerPlayer()
+    }
 
+   if (userGuess !== computerLetter && guessesLeft === 1){  
+      game.lost()     
+   }else if(userGuess === computerLetter){
+      game.won()
+   }else{
+       game.wrongGuess()
+   }
 }
